07a6ba29b17f9dade12cfb1efa47c51d87d46bd1
Loredan <loredan.bucur@siiromania.ro>
2020-05-29 17:12:46 +0300

    leptop

--- .bash_profile
+++ .bash_profile
@@ -8,8 +8,5 @@ HISTCONTROL="ignorespace:ignoredups"
 HISTIGNORE="history:ls"
 HISTSIZE=10000
 
-buildserver=10.246.6.151
-deployserver=10.246.6.122
-
 [[ -f ~/.bashrc ]] && . ~/.bashrc
 
--- setup.sh
+++ setup.sh
@@ -1,4 +1,19 @@
+sudo apt update
+sudo apt upgrade
 
+git clone https://github.com/lebucur/config
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+ln -sv config/.alias
+ln -sv config/.vimrc
+ln -sv config/.tmux.conf
+ln -sv config/.gitconfig
+
+sudo apt install gcc
+sudo apt install clang
+
+mkdir dev/c
+mkdir dev/py
+
 
