3e4c7be58387b79832d72c5d8d4cfeb09242c17b
Loredan <loredan.bucur@siiromania.ro>
2020-06-03 17:52:39 +0300

    ...

--- setup.sh
+++ setup.sh
@@ -1,25 +1,34 @@
-sed -i 's/.bash_aliases/.alias/g' .bashrc
-
-sudo apt update
-sudo apt upgrade
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/cheat/cheatsheets ~/.cheat
-
+# alias file name
+sed -i 's/.bash_aliases/.alias/g' ~/.bashrc
+ 
+# links
 ln -sv config/.alias
 ln -sv config/.vimrc
 ln -sv config/.tmux.conf
 ln -sv config/.gitconfig
 
+# source needed scripts
+source ~/.bashrc
+
+# first
+sudo apt update
+sudo apt upgrade
+ 
+# packages
 sudo apt install gcc
 sudo apt install clang
 sudo apt install python3-pip
+sudo apt install tree
+                
+# repositories
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/cheat/cheatsheets ~/.cheat
+
+# # # TLDR and TLDR++ # # #
 sudo apt install tldr
 sudo mv /usr/bin/tldr /usr/bin/tld
-
-# # # Install TLDR++ # # #
-# download the release page
+# download the release page of TLDR++
 curl -L https://github.com/isacikgoz/tldr/releases/ > .tldr++.htm
 # extract the path of the latest version for linux64
 TLDR_PATH=$(grep -m 1 _linux_amd64.tar.gz .tldr++.htm | cut -d "\"" -f2)
@@ -35,11 +44,13 @@ tar xzf $TLDR_FILE
 ~/tldr
 # # # Done # # #
 
+# pip helpers
 sudo pip3 install cheat
 sudo pip3 install eg
 sudo pip3 install manly
 
+# dir structure template
+mkdir ~/dev
 mkdir ~/dev/c
 mkdir ~/dev/py
-
 
